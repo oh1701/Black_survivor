5f814d500d6da081677a2a81c4bb0afc1543ccc0
oh1701 <rbtjd2290@naver.com>
2020-12-23 23:38:13 +0900

    1223_23:38

---
+++ app/src/main/java/com/example/black_survivor_dictinary/profilemovie.kt
@@ -0,0 +1,11 @@
+package com.example.black_survivor_dictinary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profilemovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profilemovie)
+    }
+}
