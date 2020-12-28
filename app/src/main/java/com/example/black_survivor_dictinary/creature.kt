372e0d43045c6797918882fc6fc8fb66039f7728
oh1701 <rbtjd2290@naver.com>
2020-12-28 18:45:53 +0900

    1228_18:45

--- app/src/main/java/com/example/black_survivor_dictinary/creature.kt
+++ app/src/main/java/com/example/black_survivor_dictinary/creature.kt
@@ -3,9 +3,9 @@ package com.example.black_survivor_dictinary
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Character_talk : AppCompatActivity() {
+class creature : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_character_talk)
+        setContentView(R.layout.activity_creature)
     }
 }
