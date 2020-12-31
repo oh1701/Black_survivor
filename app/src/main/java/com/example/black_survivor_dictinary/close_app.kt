06093263f03028656ffe25b5cdb1d7ca4006a568
oh1701 <rbtjd2290@naver.com>
2020-12-31 13:51:50 +0900

    1231_commit

---
+++ app/src/main/java/com/example/black_survivor_dictinary/close_app.kt
@@ -0,0 +1,11 @@
+package com.example.black_survivor_dictinary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class close_app : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_close_app)
+    }
+}
